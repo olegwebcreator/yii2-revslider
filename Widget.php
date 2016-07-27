51f42e84a5d249cc6e38f04fc9a50db4d1be3c29
System <oleg.webcreator@yandex.ru>
2016-07-27 23:20:13 +0300

    added innerOptions

--- Widget.php
+++ Widget.php
@@ -11,8 +11,53 @@ use yii\helpers\Json;
  */
 class Widget extends \yii\base\Widget
 {
+    /**
+     * @var boolean whether the [Modernizr JavaScript library](http://modernizr.com) should be registered.
+     */
+    public $registerModernizr = true;
+
+    /**
+     * @var array the HTML attributes for the widget container tag.
+     * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
+     */
+    public $options = ['class' => 'fullscreenbanner-container'];
+
+    /**
+     * @var array the HTML attributes for widget inner container tag.
+     * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
+     */
+    public $innerOptions = ['class' => 'fullscreenbanner'];
+
+    /**
+     * @var array
+     */
+    public $clientOptions = [];
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        if (!isset($this->options['id'])) {
+            $this->options['id'] = $this->id;
+        }
+        if (isset($this->clientOptions['current']) && $this->clientOptions['current'] > 0) {
+            $this->clientOptions['current'] = $this->clientOptions['current'] - 1;
+        }
+        ob_start();
+    }
+
+    /**
+     * Render Widget
+     */
     public function run()
     {
-        return "Hello!";
+        $content = ob_get_clean();
+        echo Html::beginTag('div', $this->options) . "\n";
+        echo Html::beginTag('div', $this->innerOptions) . "\n";
+        echo $content;
+        echo Html::endTag('div') . "\n";
+        echo Html::endTag('div') . "\n";
     }
 }
