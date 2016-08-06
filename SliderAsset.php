b9239a51f67714dd2848db586856f2d82a04dc37
System <oleg.webcreator@yandex.ru>
2016-08-06 04:56:18 +0300

    no message

--- SliderAsset.php
+++ SliderAsset.php
@@ -9,6 +9,7 @@ class SliderAsset extends AssetBundle
 {
     public $sourcePath = '@webcreator/revslider/assets';
     public $js = [
+        'plugins/rs-plugin/js/jquery.themepunch.tools.min.js',
         'plugins/rs-plugin/js/jquery.themepunch.revolution.min.js',
         'custom.js'
     ];
