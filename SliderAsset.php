9ff77bc25cf807c19643d60b443268e9ae40e040
System <oleg.webcreator@yandex.ru>
2016-08-06 04:28:27 +0300

    no message

--- SliderAsset.php
+++ SliderAsset.php
@@ -7,7 +7,12 @@ use yii\web\View;
 
 class SliderAsset extends AssetBundle
 {
-    //public $sourcePath = '@webcreator/yii2-revslider/assets';
+    public $sourcePath = '@webcreator/yii2-revslider/assets';
+    public $js = [
+        'plugins/jquery-ui/jquery-ui.js',
+        'plugins/rs-plugin/js/jquery.themepunch.tools.min.js',
+        'plugins/rs-plugin/js/jquery.themepunch.revolution.min.js'
+    ];
     public $depends = [
         'yii\web\JqueryAsset'
     ];
